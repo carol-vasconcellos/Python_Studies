08ba8912b212c3ee468245ebea9e1b8dcb6537ae
Ana Caroline Vasconcellos <anawold2008@gmail.com>
2025-09-13 19:46:40 +0000

    day-one

--- daily-studies/day2.py
+++ daily-studies/day2.py
@@ -4,9 +4,9 @@ a = int(input("Digite o primeiro número: "))
 b = int(input("Digite o segundo número: "))
 c = int(input("Digite o terceiro número: "))
 
-if a => b and a => c:
+if a >= b and a >= c:
   print("Numero maior: ", a)
-elif b => a and b => c:
+elif b >= a and b >= c:
   print("Numero maior: ", b)
 else: 
   print("Numero maior: ", c)
@@ -14,9 +14,12 @@ else:
 
 #Exercício 2 – Par ou Ímpar
 
-numero = int(input("Digite o número: "))
+def even_or_odd(numero):
+  
+  if numero % 2 == 0:
+    print("O número é par")
+  else:
+    print("O número é impar")
 
-if numero % 2 == 0:
-  print("O número é impar")
-else:
-  print("O número é par")
+num = int(input("Digite o número: "))
+even_or_odd(num)
